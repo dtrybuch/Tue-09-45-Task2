80361f19597800ecc3072f469839f7b170111d01
dtrybuch <dominik.trybuch@gmail.com>
2020-03-31 11:22:51 +0200

    Add solution

--- task.py
+++ task.py
@@ -28,3 +28,76 @@
 #
 #Delete these comments before commit!
 #Good luck.
+class Diary:
+    def __init__(self, students):
+        self.students = students
+    
+
+    def get_students_grades(self,name,surname):
+        for student in self.students:
+            if student.name == name and student.surname == surname:
+                return student.grades
+
+
+    def get_student(self, name, surname):
+        for student in self.students:
+            if student.name == name and student.surname == surname:
+                return student
+
+
+    def get_students_average(self):
+        sum = 0
+        len = 0
+        for student in self.students:
+            for grade in student.grades:
+                sum = sum + grade
+                len = len + 1
+        if len == 0:
+            return 0
+        return sum/len
+
+
+    def get_student_average(self, name, surname):
+        student = self.get_student(name,surname)
+        sum = 0
+        for grade in student.grades:
+            sum = sum + grade
+        if len(student.grades) == 0:
+            return 0
+        return sum/len(student.grades)
+
+
+    def add_student(self, student):
+        students.append(student)
+
+
+    def add_grade_for_student(self, name, surname, grade):
+        student = self.get_student(name,surname)
+        student.add_grade(grade)
+
+
+class Student:
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.grades = []
+
+        
+    def add_grade(self, grade):
+        self.grades.append(grade)
+
+if __name__ == "__main__":
+    students = []
+    students.append(Student('Jan','Kowalski'))
+    diary = Diary(students) 
+    diary.add_grade_for_student('Jan','Kowalski',5)
+    diary.add_grade_for_student('Jan','Kowalski',2)
+    diary.add_grade_for_student('Jan','Kowalski',1)
+    diary.add_student(Student('Jan','Wozniak'))
+    diary.add_grade_for_student('Jan', 'Wozniak',5)
+    print(diary.get_students_grades('Jan','Kowalski'))
+
+    print(diary.get_students_average())
+
+    print(diary.get_student_average('Jan','Wozniak'))
